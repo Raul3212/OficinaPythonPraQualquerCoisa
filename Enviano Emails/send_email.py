ca7ac436cecb49d5ad5cc1579761247ca8eea3b1
Raul de Araújo <rauldearaujolima@gmail.com>
2017-09-10 21:59:28 -0300

    Enviando Emails

---
+++ Enviano Emails/send_email.py
@@ -0,0 +1,35 @@
+import smtplib
+from email.mime.multipart import *
+from email.mime.text import *
+from email.mime.base import *
+from email import encoders
+
+fromaddr = 'rauldearaujolima@gmail.com'
+toaddrs  = 'rauldearaujolima@gmail.com'
+
+msg = 	MIMEMultipart()
+msg['From'] = fromaddr
+msg['To'] = toaddrs
+msg['Subject'] = 'SUBJECT OF THE MAIL'
+ 
+body = 'YOUR MESSAGE HERE'
+msg.attach(MIMEText(body, 'plain'))
+
+filename = 'jp2b16.jpg'
+attachment = open('jp2b16.jpg', 'rb')
+
+part = MIMEBase('application', 'octet-stream')
+part.set_payload((attachment).read())
+encoders.encode_base64(part)
+part.add_header('Content-Disposition', 'attachment; filename= %s' % filename)
+ 
+msg.attach(part)
+
+username = 'rauldearaujolima@gmail.com'
+password = open('senha.txt', 'r').readline()
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.ehlo()
+server.starttls()
+server.login(username, password)
+server.sendmail(fromaddr, toaddrs, msg.as_string())
+server.quit()
